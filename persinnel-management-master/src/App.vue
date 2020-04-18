2ef2eeaa7c11dee92f948e5bbce29ec29c7587d9
YuxinMou <36131611+meimeity@users.noreply.github.com>
2020-04-18 20:51:57 +0800

    Add files via upload

---
+++ persinnel-management-master/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="less">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  height: 100%;
+}
+
+body, html {
+  height: 100%;
+  margin: 0;
+  padding: 0
+}
+ol, ul {
+list-style-image:none;
+list-style-position:outside;
+list-style-type:none;
+}
+
+body, dl, dt, dd, ul, ol, li, pre, form, fieldset, input, p, blockquote, th, td {
+font-weight:400;
+margin:0;
+padding:0;
+}
+h1, h2, h3, h4, h4, h5 {
+margin:0;
+padding:0;
+}
+
+</style>
---
+++ persinnel-management-master/src/views/Login/Login.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="login">
+    <div class="login-box">
+      <div class="logo">
+        <img src="../../assets/logo.png" alt="logo" style="width: 400px">
+      </div>
+      <div class="form">
+        <div class="form-title">
+          <div class="title">用户登录</div>
+          <div>一天的开始</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login {
+  height: 100%;
+  background:  url('../../assets/login-bg.png') no-repeat;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .login-box {
+    width: 800px;
+    height: 400px;
+    display: flex;
+    .logo {
+      background: #fff;
+      display: flex;
+      align-items: center;
+    }
+    .form {
+      width: 100%;
+      border-left: 1px solid #333;
+      background: #fff;
+      .title {
+        color:#7F7F7F;
+        font-size:36px
+      }
+    }
+  }
+}
+</style>
